5ed1f36115dfd16a07b79c2623805a37bc98b7a5
JewishStew <70977089+ZackHagerty@users.noreply.github.com>
2022-09-10 15:14:39 -0400

    Bunch o' metadata
    Plus an assignment

---
+++ SE320/Assignment-1/src/assignmentPackage/twoIntegerSum.java
@@ -0,0 +1,35 @@
+package assignmentPackage;
+import java.util.*;
+
+public class twoIntegerSum {
+	
+	
+	public static void main(String[] args) {
+		
+		boolean continueInput = true;
+		int number1 = 0;
+		int number2 = 0;
+		int sum = 0;
+		Scanner input = new Scanner(System.in);	
+		
+		do {
+			try {
+				System.out.print("Enter two integers: ");
+				number1 = input.nextInt();
+				number2 = input.nextInt();
+				
+				sum = number1 + number2;
+				
+				System.out.println("The sum of the numbers entered is, " + sum + "\n\n");
+			
+				continueInput = false;
+			}
+			catch (InputMismatchException ex) {
+				System.out.println("\n \nTry again .(" +
+						"Incorrect input: two integers are required)\n");
+				input.nextLine();
+			}
+		} while(continueInput);
+
+	}
+}
